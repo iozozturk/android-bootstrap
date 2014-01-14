b8d49fdcd0ab97d19db3aa74ae0f60c0662f5c82
ismet <ismet@ifountain.com>
2014-01-14 19:55:53 +0200

    social primitive working

--- app/src/main/java/com/donnfelker/android/bootstrap/ui/PreferenceListAdapter.java
+++ app/src/main/java/com/donnfelker/android/bootstrap/ui/PreferenceListAdapter.java
@@ -2,12 +2,17 @@ package com.donnfelker.android.bootstrap.ui;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.donnfelker.android.bootstrap.R;
+import com.donnfelker.android.bootstrap.core.Constants;
 import com.donnfelker.android.bootstrap.core.Preference;
+import com.donnfelker.android.bootstrap.util.Strings;
 
 import java.util.List;
 
@@ -22,25 +27,43 @@ public class PreferenceListAdapter extends BaseAdapter {
     PreferenceListAdapter(Activity activity, List<Preference> preferenceList){
         inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.preferenceList = preferenceList;
+        Log.d(Constants.IMeetapp.Log,"Preference List Adapter constructed...");
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return preferenceList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return preferenceList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        Log.d(Constants.IMeetapp.Log,"Building Preference view..");
+        View vi = convertView;
+          if (convertView == null)
+              vi = inflater.inflate(R.layout.social_list_item, null); // create layout from
+
+        TextView textView = null; // preference name
+        if (vi != null) {
+            textView = (TextView) vi.findViewById(R.id.social_item_title);
+        }
+
+        Preference preference = preferenceList.get(position);
+
+        if (textView != null) {
+            textView.setText(preference.getName());
+        }
+        String isViewNull = vi != null ? "view is not null": "view is null";
+        Log.d(Constants.IMeetapp.Log,"Returning Preference view for " + preference.getName() + " and " + isViewNull);
+        return vi;
     }
 }
--- app/src/main/java/com/donnfelker/android/bootstrap/ui/SocialActivity.java
+++ app/src/main/java/com/donnfelker/android/bootstrap/ui/SocialActivity.java
@@ -1,7 +1,49 @@
 package com.donnfelker.android.bootstrap.ui;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.donnfelker.android.bootstrap.R;
+import com.donnfelker.android.bootstrap.core.Constants;
+import com.donnfelker.android.bootstrap.core.Preference;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ismet on 1/14/14.
  */
-public class SocialActivity {
+public class SocialActivity extends BootstrapActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(Constants.IMeetapp.Log, "Creating Social Activity..");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social);
+
+        setTitle(R.string.social);
+
+        final List<Preference> preferenceList = new ArrayList<Preference>();
+        preferenceList.add(new Preference("Java",3, 5));
+        preferenceList.add(new Preference("C", 3, 5));
+        preferenceList.add(new Preference("C++", 3, 5));
+        preferenceList.add(new Preference("Experience", 3, 5));
+        preferenceList.add(new Preference("School", 3, 5));
+
+        ListView customListView = (ListView) findViewById(R.id.social_list_view);
+        PreferenceListAdapter preferenceListAdapter = new PreferenceListAdapter(this, preferenceList);
+        customListView.setAdapter(preferenceListAdapter);
+
+        customListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), preferenceList.get(position).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+        Log.d(Constants.IMeetapp.Log, "Created Social Activity..");
+
+    }
 }
